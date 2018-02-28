f714cc0988c0b679f374aa13581967db28e97079
shashank <shashank2598@gmail.com>
2018-02-28 08:40:51 -0800

    heroku static

--- centre/settings/production.py
+++ centre/settings/production.py
@@ -56,7 +56,6 @@ INSTALLED_APPS = [
     'doubts.apps.DoubtsConfig',
     'tests.apps.TestsConfig',
     'Users.apps.UsersConfig',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'widget_tweaks',
 ]
 
