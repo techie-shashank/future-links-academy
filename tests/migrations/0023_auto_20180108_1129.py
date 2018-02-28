356c85283614766f2c63286665fbfb1036c41eac
shashank <shashank2598@gmail.com>
2018-02-28 08:01:49 -0800

    Heroku third commit

--- tests/migrations/0023_auto_20180108_1129.py
+++ tests/migrations/0023_auto_20180108_1129.py
@@ -15,12 +15,6 @@ class Migration(migrations.Migration):
 
         migrations.AddField(
             model_name='question',
-            name='id',
-            field=models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID'),
-            preserve_default=False,
-        ),
-        migrations.AddField(
-            model_name='question',
             name='number',
             field=models.IntegerField(default=0),
         ),
