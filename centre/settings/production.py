0b2bff87a2303cfd8130039d26d8d6fce00f4ba7
shashank <--global>
2020-06-08 20:35:29 +0530

    minor fixes

--- centre/settings/production.py
+++ centre/settings/production.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'tests.apps.TestsConfig',
     'Users.apps.UsersConfig',
     'notes.apps.NotesConfig',
+    'study_material.apps.StudyMaterialConfig',
     'widget_tweaks',
 ]
 
