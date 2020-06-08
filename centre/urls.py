ae7fe782a50bcdc7b82b7ff38df891fa0dfb541d
shashank <--global>
2020-06-08 20:57:14 +0530

    django admin static issue

--- centre/urls.py
+++ centre/urls.py
@@ -37,6 +37,5 @@ urlpatterns = [
     url(r'^study/material/', include("study_material.urls", namespace="study_material")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
