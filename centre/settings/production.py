970543fd445f52505f393f10f4caa46baccf0df2
shashank <shashank2598@gmail.com>
2020-06-20 14:33:58 +0530

    static file settings

--- centre/settings/production.py
+++ centre/settings/production.py
@@ -53,13 +53,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
+    'storages',
     'blog.apps.BlogConfig',
     'doubts.apps.DoubtsConfig',
     'tests.apps.TestsConfig',
     'Users.apps.UsersConfig',
     'notes.apps.NotesConfig',
     'study_material.apps.StudyMaterialConfig',
-    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -156,7 +157,7 @@ USE_TZ = True
 STATIC_ROOT = normpath(join(BASE_DIR, 'static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
@@ -182,4 +183,16 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# DEFAULT_FILE_STORAGE = 'libs.s3.MediaFilesStorage'
+# STATICFILES_STORAGE = 'libs.s3.HerokuStaticS3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = '{0}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# s3 static settings
+AWS_LOCATION = 'static'
+STATIC_URL = 'https://{0}/{1}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
