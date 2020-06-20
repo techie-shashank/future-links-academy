8d124c15a48f585c438cdc6859fa65cd849319ba
shashank <shashank2598@gmail.com>
2020-06-20 15:04:24 +0530

    testing

--- centre/settings/production.py
+++ centre/settings/production.py
@@ -71,7 +71,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'centre.urls'
