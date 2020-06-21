837e34b06ab31ce3cf095408eb0360da4a11437d
shashank <shashank2598@gmail.com>
2020-06-21 10:56:01 +0530

    updated util

--- centre/utils.py
+++ centre/utils.py
@@ -5,7 +5,7 @@ def get_default_django_settings_module():
     try:
         file_ = imp.find_module('local', ['centre/settings'])[0]
     except ImportError:
-        default_django_settings_module = "centre.settings.base"
+        default_django_settings_module = "centre.settings.production"
     else:
         default_django_settings_module = "centre.settings.local"
         file_.close()
