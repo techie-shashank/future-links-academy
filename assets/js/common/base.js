7aa2399704fd792687e0498b5debd4645ba3c398
shashank <shashank2598@gmail.com>
2020-06-21 12:55:45 +0530

    added footer

--- assets/js/common/base.js
+++ assets/js/common/base.js
@@ -3,3 +3,30 @@ $('.dropdown-submenu a.test').on("click", function(e){
     e.stopPropagation();
     e.preventDefault();
 });
+
+$(document).ready(function() {
+
+     $("footer a[href='#body']").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
